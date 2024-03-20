a34bf55487b158ccefac412e4eda45fe14d3a8ee
abdullahihussein1995 <abdullahi.hussein945@gmail.com>
2024-03-20 19:49:29 +0300

    changes

--- src/WeatherDisplay.jsx
+++ src/WeatherDisplay.jsx
@@ -3,18 +3,17 @@
 import React from 'react';
 
 function WeatherDisplay({ weather }) {
-  if (!weather) return null;
+  const iconUrl = `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`;
 
   return (
     <div>
       <h2>{weather.name}, {weather.sys.country}</h2>
+      <img src={iconUrl} alt="Weather icon" />
       <p>Temperature: {weather.main.temp}°C</p>
       <p>Condition: {weather.weather[0].main} ({weather.weather[0].description})</p>
       <p>Humidity: {weather.main.humidity}%</p>
       <p>Wind Speed: {weather.wind.speed} m/s</p>
-      {/* Add more weather details as desired */}
     </div>
   );
 }
-
 export default WeatherDisplay;
